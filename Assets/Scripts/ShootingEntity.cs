fdbce48322bc08a7c4640ef79bef32c007a879f5
박원준T. <84364855+sungil-i@users.noreply.github.com>
2026-09-22 08:47:15 +0900

    Update ShootingEntity.cs

--- Assets/Scripts/ShootingEntity.cs
+++ Assets/Scripts/ShootingEntity.cs
@@ -1,20 +1,5 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-// [교육용 스크립트] ShootingEntity
-// ------------------------------------------------------------
-// 플레이어(PlayerShooter), 적(EnemyShooter) 등 "총알을 쏘고 맞는
-// 개체"들이 공통으로 사용할 기능을 모아 둔 부모(기반) 클래스입니다.
-//
-// 이 프로젝트는 초보자 학습용이므로 abstract(추상) 키워드를 사용하지
-// 않는 일반 class로 선언했습니다. abstract 클래스는 직접 게임
-// 오브젝트에 붙일 수 없어 개념이 다소 어렵게 느껴질 수 있기 때문에,
-// 여기서는 이해하기 쉬운 "공통 부모 클래스(공용 설계도)"로만
-// 사용합니다.
-//
-// 모든 필드를 public으로 선언한 이유도 같습니다. private나
-// [SerializeField] 같은 접근 제한자는 캡슐화(정보 은닉) 개념이
-// 필요한데, 처음 배우는 단계에서는 인스펙터(Inspector) 창에서 값을
-// 바로 보고 수정할 수 있도록 public으로 단순화했습니다.
 public class ShootingEntity : MonoBehaviour
 {
     // 이동 속도. 자식 클래스(PlayerShooter, EnemyShooter)가 이동
